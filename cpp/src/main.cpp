7188f7a3db452219b9777e6a8aa2627407755cf2
Magius(CHE) <magiusche@magius.it>
2022-10-11 11:59:51 +0200

    cpp: aligned signature to rust and clamp 4.

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2022, Magius(CHE)
+ *
+ * This software is provided "as-is", without any express or implied warranty. In no event
+ * will the authors be held liable for any damages arising from the use of this software.
+ * Read the LICENSE file for more details.
+ *
+ * @author: Magius(CHE) - magiusche@magius.it
+ */
+
 #include <iostream>
 #include <filesystem>
 #include <vector>
@@ -29,13 +39,37 @@ namespace po = boost::program_options;
 #define color_string$(color) \
     "\u001B[38;5;" #color "m"
 
-#define color_clear_string$ \
-    "\u001b[0m"
+#define bold_string$ \
+    "\u001B[1m"
+
+#define underline_string$ \
+    "\u001B[4m"
+
+#define clear_string$ \
+    "\u001B[0m"
 
 // https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
-#define C_TITLE color_string$(130)
-#define C_OPT color_string$(107)
-#define C_LS color_clear_string$
+//#define C_TITLE color_string$(130)
+#define C_TITLE bold_string$ underline_string$
+//#define C_OPT color_string$(107)
+#define C_OPT bold_string$
+#define C_LS clear_string$
+
+#ifndef PROJECT_NAME
+#define PROJECT_NAME "steam-shortcuts-editor"
+#endif
+#ifndef PROJECT_VERSION
+#define PROJECT_VERSION "custom"
+#endif
+#ifndef PROJECT_AUTHOR_NAME
+#define PROJECT_AUTHOR_NAME "Magius(CHE)"
+#endif
+#ifndef PROJECT_AUTHOR_EMAIL
+#define PROJECT_AUTHOR_NAME "magiusche@magius.it"
+#endif
+#ifndef PROJECT_HOMEPAGE
+#define PROJECT_HOMEPAGE "https://github.com/magiusche/steam-shortcuts-editor"
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -44,24 +78,29 @@ int main(int argc, char *argv[])
 
     try
     {
-        string ifile;
+        string argument_01;
         string command;
         string separator = " ";
         unordered_map<string, string>
             format_switches;
 
         auto help_main =
-            "VDF Shortcuts Editor for Steam Client\n\n" C_TITLE "USAGE:\n" C_LS
-            "    steam-shortcuts-editor <SHORTCUTS_PATH> <SUBCOMMAND>\n\n" C_TITLE "ARGS:\n" C_OPT "    <SHORTCUTS_PATH>" C_LS "    Path to \"shortcuts.vdf\"\n\n" C_TITLE "SUBCOMMANDS:\n" C_OPT "    help" C_LS "       Print this message or the help of the given subcommand(s)\n" C_OPT "    version" C_LS "    Print version information\n" C_LS;
+            "VDF Shortcuts Editor for Steam Client\n\n" C_TITLE "Usage:" C_LS
+            " steam-shortcuts-editor <COMMAND>\n\n" C_TITLE "Commands:" C_LS "\n"
+            "    " C_OPT "list" C_LS "     List entries summary info\n"
+            "    " C_OPT "version" C_LS "  Print version information\n"
+            "    " C_OPT "help" C_LS "     Print this message or the help of the given subcommand(s)\n"
+            "\n" C_TITLE "Options:" C_LS "\n"
+            "    " C_OPT "-h, --help" C_LS "  Print this message.\n";
 
         po::options_description hidden("Hidden options");
         // desc.add_options()("help,h", "Print help information");
         // desc.add_options()("list", "List entries summary info");
-        hidden.add_options()("input", po::value(&ifile), "");
         hidden.add_options()("command", po::value(&command), "");
+        hidden.add_options()("argument_01", po::value(&argument_01), "");
         po::positional_options_description positional;
-        positional.add("input", 1);
-        positional.add("command", -1);
+        positional.add("command", 1);
+        positional.add("argument_01", -1);
 
         hidden.add_options()("separator", po::value(&separator), "Table output columns separator");
         hidden.add_options()("keys", "Show key for each value in table output");
@@ -94,45 +133,52 @@ int main(int argc, char *argv[])
         po::store(po::command_line_parser(argc, argv).options(hidden).positional(positional).run(), vm);
         po::notify(vm);
 
-        fmt::print("Input: {}\n", ifile);
+#if DEBUG==1
+        fmt::print("argument_01: {}\n", argument_01);
         fmt::print("Command: {}\n", command);
+#endif
 
-        if (boost::iequals(ifile, "version"))
+        if (boost::iequals(argument_01, "version"))
         {
             // print spcific command
-            fmt::print("{} {}\n\n", "steam-shortcuts-editor", "0.1.0");
+            fmt::print("{} {} by {} - {}, {}\n\n", PROJECT_NAME, PROJECT_VERSION, PROJECT_AUTHOR_NAME, PROJECT_AUTHOR_EMAIL, PROJECT_HOMEPAGE);
             return 0;
         }
 
-        if (!vm.count("command") || boost::iequals(command, "help"))
+        if (boost::iequals(command, "help") && !vm.count("argument_01"))
         {
             cout << help_main << "\n\n";
             return 0;
         }
 
-        if (boost::iequals(ifile, "help"))
+        if (boost::iequals(command, "help"))
         {
-            if (boost::iequals(command, "list"))
+            if (boost::iequals(argument_01, "list"))
             {
                 // print spcific command
-                fmt::print("List entries summary info\n\n" C_TITLE "USAGE:\n" C_LS
-                           "    steam-shortcuts-editor <SHORTCUTS_PATH> list <ARGS>\n\n" C_TITLE "ARGS:\n" C_LS);
+                fmt::print("List entries summary info\n\n" C_TITLE "Usage:\n" C_LS " steam-shortcuts-editor list [OPTIONS] <SHORTCUTS_PATH>\n\n"
+                           C_TITLE "Arguments:\n" C_LS
+                           "  <SHORTCUTS_PATH>  Path to \"shortcuts.vdf\"\n\n"
+                           C_TITLE "Options:\n" C_LS);
+
+                fmt::print("    " C_OPT "--separator <SEPARATOR>\n" C_LS "       Table output columns separator [default: \" \"]\n\n");
+                fmt::print("    " C_OPT "--keys\n" C_LS "       Show key for each value in table output\n\n");
                 for (const auto &elem : format_switches)
                 {
-                    fmt::print("    " C_OPT "--{} <format>\n" C_LS "       {}\n\n", elem.first, hidden.find(elem.first,false,true).description());
+                    fmt::print("    " C_OPT "--{} <format>\n" C_LS "       {}\n\n", elem.first, hidden.find(elem.first, false, true).description());
                 }
                 return 0;
             }
             else
             {
-                throw invalid_argument("Help command must be followed by a subcommand.");
+                throw invalid_argument("Help command must be followed by a command to inspect.");
             }
         }
 
-        if (ifile.empty())
-            throw invalid_argument("Missing input shortcuts file.");
+        if (argument_01.empty())
+            throw invalid_argument("Missing input shortcuts file. Check the usage.");
 
-        const fs::path vdf(ifile);
+        const fs::path vdf(argument_01);
         if (!fs::exists(vdf))
         {
             throw runtime_error(fmt::format("Shortcuts file not exists at: \"{}\"", vdf.string()));
--- cpp/src/shortcuts.hpp
+++ cpp/src/shortcuts.hpp
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2022, Magius(CHE)
+ *
+ * This software is provided "as-is", without any express or implied warranty. In no event
+ * will the authors be held liable for any damages arising from the use of this software.
+ * Read the LICENSE file for more details.
+ *
+ * @author: Magius(CHE) - magiusche@magius.it
+ */
+
 #pragma once
 
 #include <iostream>
